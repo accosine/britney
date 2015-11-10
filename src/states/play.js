8bb96333a08af4ae2c00b40cf898360fc1d1b04a
Mirco Ohl <mircoohl@gmail.com>
2015-11-10 21:54:12 +0000

    Second tap on a tower destroys it

--- src/states/play.js
+++ src/states/play.js
@@ -12,7 +12,6 @@ const ROWS = 12;
 
 let towerGroup;
 let pathGroup;
-let button;
 let map = [];
 
 export default class Play extends Phaser.State {
@@ -21,7 +20,8 @@ export default class Play extends Phaser.State {
     this.game.input.onTap.add(this.handleTap);
     towerGroup = this.game.add.group();
     pathGroup = this.game.add.group();
-    button = this.game.add.button(620, 300, 'tower', this.actionOnClick, this, 2, 1, 0);
+    this.game.add.button(620, 300, 'tower', this.startTheFuckingPathfinding, this, 2, 1, 0);
+    this.game.add.button(620, 365, 'tower', this.clearEveryFuckingThing, this, 2, 1, 0);
 
     this.game.stage.backgroundColor = 0x00a000;
     this.game.CELL_WIDTH = 600 / COLUMNS;
@@ -35,7 +35,20 @@ export default class Play extends Phaser.State {
     }
   }
 
-  actionOnClick = () => {
+  // TODO: get rid of "global" onTap handler
+  handleTap = (pointer) => {
+    const row = Math.floor( pointer.x / this.game.CELL_WIDTH );
+    const column = Math.floor( pointer.y / this.game.CELL_HEIGHT );
+    if (row > 11 ) return;
+    console.log(`Row: ${row}\t Column: ${column}`);
+
+    if(!map[row][column]) {
+      map[row][column] = 1;
+      this.insertTower(row, column);
+    }
+  }
+
+  startTheFuckingPathfinding = () => {
     console.log('START!💩');
 
     easystar.setGrid(map);
@@ -53,38 +66,39 @@ export default class Play extends Phaser.State {
     easystar.calculate();
   }
 
-  insertTower = (row, col) => {
-    towerGroup.create(row * this.game.CELL_WIDTH, col * this.game.CELL_HEIGHT, 'tower');
-    towerGroup.children[towerGroup.length - 1].width = this.game.CELL_WIDTH - 3;
-    towerGroup.children[towerGroup.length - 1].height = this.game.CELL_HEIGHT - 3;
-  }
-
-  insertPathPoint = (row, col) => {
-    const offset = this.game.CELL_WIDTH / 2 - (this.game.CELL_WIDTH - 40) / 2;
-    pathGroup.create(row * this.game.CELL_WIDTH + offset, col * this.game.CELL_HEIGHT + offset, 'tower');
-    pathGroup.children[pathGroup.length - 1].width = this.game.CELL_WIDTH - 40;
-    pathGroup.children[pathGroup.length - 1].height = this.game.CELL_HEIGHT - 40;
-  }
-
-  handleTap = (pointer) => {
-    const row = Math.floor( pointer.x / this.game.CELL_WIDTH );
-    const column = Math.floor( pointer.y / this.game.CELL_HEIGHT );
-    if (row > 11 ) return;
-    console.log(`Row: ${row}\t Column: ${column}`);
-
-    if(map[row][column]) {
+  clearEveryFuckingThing = () => {
+      // data structure for path finding
       for (var i = 0; i < COLUMNS; i++) {
         for (var j = 0; j < ROWS; j++) {
           map[i][j] = 0;
         }
       }
+      // graphic objects
       towerGroup.removeAll();
       pathGroup.removeAll();
-    }
-    else {
-      map[row][column] = 1;
-      this.insertTower(row, column);
-    }
+  }
+
+  insertTower = (row, col) => {
+    let tower = towerGroup.create(row * this.game.CELL_WIDTH, col * this.game.CELL_HEIGHT, 'tower');
+    tower.inputEnabled = true;
+    tower.events.onInputUp.add(this.handleTowerTap, this);
+    towerGroup.children[towerGroup.length - 1].width = this.game.CELL_WIDTH - 3;
+    towerGroup.children[towerGroup.length - 1].height = this.game.CELL_HEIGHT - 3;
+  }
+
+  handleTowerTap = (tower) => {
+    const row = Math.floor( tower.x / this.game.CELL_WIDTH );
+    const column = Math.floor( tower.y / this.game.CELL_HEIGHT );
+    tower.destroy();
+    map[row][column] = 0;
+  }
+
+  // helper method, only used to show path while debugging
+  insertPathPoint = (row, col) => {
+    const offset = this.game.CELL_WIDTH / 2 - (this.game.CELL_WIDTH - 40) / 2;
+    pathGroup.create(row * this.game.CELL_WIDTH + offset, col * this.game.CELL_HEIGHT + offset, 'tower');
+    pathGroup.children[pathGroup.length - 1].width = this.game.CELL_WIDTH - 40;
+    pathGroup.children[pathGroup.length - 1].height = this.game.CELL_HEIGHT - 40;
   }
 
 }
