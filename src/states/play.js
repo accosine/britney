d2c62e6e29d06e93c9d10254889ed1b3089b8bcd
Mirco Ohl <mircoohl@gmail.com>
2015-11-23 22:30:46 +0000

    Get your effin' coordinates right (and let a beholder walk it)

--- src/states/play.js
+++ src/states/play.js
@@ -7,68 +7,77 @@ easystar.setAcceptableTiles([0]);
 //easystar.enableDiagonals();
 easystar.enableCornerCutting();
 
-const COLUMNS = 12;
-const ROWS = 12;
-
-let towerGroup;
-let pathGroup;
-let map = [];
-
 export default class Play extends Phaser.State {
 
-  create() {
-    //this.game.physics.startSystem(Phaser.Physics.ARCADE);
-    let beholder = this.game.add.sprite(50, 50, 'rbeholder');
-    beholder.anchor.setTo(0.5, 0.5);
-    beholder.animations.add('walk', Phaser.Animation.generateFrameNames('beholder/walk/', 0, 1, '', 1), 5, true, false);
-    beholder.animations.play('walk');
+  constructor() {
+    super();
+    this.towerGroup;
+    this.pathGroup;
+    this.beholder;
+    this.map = [];
+    this.COLUMNS = 12;
+    this.ROWS = 12;
+  }
 
-    this.game.physics.arcade.enable([ beholder ], Phaser.Physics.ARCADE);
-    this.game.add.tween(beholder.body).to( { y: 300 }, 3000, Phaser.Easing.Linear.None, true);
+  create() {
+    this.beholder = this.game.add.sprite(50, 50, 'rbeholder');
+    this.beholder.anchor.setTo(0.5, 0.5);
+    this.beholder.animations.add('walk',
+      Phaser.Animation.generateFrameNames('beholder/walk/', 0, 1, '', 1), 5, true, false);
+    this.beholder.animations.play('walk');
+
+    this.game.physics.arcade.enable([ this.beholder ], Phaser.Physics.ARCADE);
 
     this.game.input.onTap.add(this.handleTap);
-    towerGroup = this.game.add.group();
-    pathGroup = this.game.add.group();
+    this.towerGroup = this.game.add.group();
+    this.pathGroup = this.game.add.group();
     this.game.add.button(620, 300, 'tower', this.startTheFuckingPathfinding, this, 2, 1, 0);
     this.game.add.button(620, 365, 'tower', this.clearEveryFuckingThing, this, 2, 1, 0);
 
     this.game.stage.backgroundColor = 0x00a000;
-    this.game.CELL_WIDTH = 600 / COLUMNS;
-    this.game.CELL_HEIGHT = 600 / ROWS;
+    this.game.CELL_WIDTH = 600 / this.COLUMNS;
+    this.game.CELL_HEIGHT = 600 / this.ROWS;
 
-    for (var col = 0; col < COLUMNS; col++) {
-      map.push([]);
-      for (var row = 0; row < ROWS; row++) {
-        map[col][row] = 0;
+    for (var row = 0; row < this.ROWS; row++) {
+      this.map.push([]);
+      for (var col = 0; col < this.COLUMNS; col++) {
+        this.map[row][col] = 0;
       }
     }
   }
 
   // TODO: get rid of "global" onTap handler
   handleTap = (pointer) => {
-    const row = Math.floor( pointer.x / this.game.CELL_WIDTH );
-    const column = Math.floor( pointer.y / this.game.CELL_HEIGHT );
-    if (row > 11 ) return;
-    console.log(`Row: ${row}\t Column: ${column}`);
+    const [col, row] = this.pixelsToCells(pointer.x, pointer.y);
+    if (col > 11 ) return;
+    console.log(`Column: ${col}\t Row: ${row}`);
 
-    if(!map[row][column]) {
-      map[row][column] = 1;
-      this.insertTower(row, column);
+    if (!this.map[row][col]) {
+      this.map[row][col] = 1;
+      this.insertTower(col, row);
     }
+
   }
 
   startTheFuckingPathfinding = () => {
     console.log('START!💩');
 
-    easystar.setGrid(map);
+    easystar.setGrid(this.map);
     easystar.findPath(0, 0, 11, 11, (path) => {
-      pathGroup.removeAll();
+      let tweenPoints = { x: [], y: [] };
+      const length = path.length;
+      const speed = 1000;
+      this.pathGroup.removeAll();
       if (path === null) {
         console.log('The path to the destination point was not found.');
       } else {
         for (var i = 0; i < path.length; i++) {
-          this.insertPathPoint(path[i].y, path[i].x);
+          this.insertPathPoint(path[i].x, path[i].y);
+          const [x, y] = this.cellsToPixels(path[i].x, path[i].y);
+          tweenPoints.x.push(x + 18);
+          tweenPoints.y.push(y + 18);
         }
+        this.game.add.tween(this.beholder.body).to(tweenPoints, length * speed, Phaser.Easing.Linear.None, true);
       }
     });
 
@@ -77,37 +86,47 @@ export default class Play extends Phaser.State {
 
   clearEveryFuckingThing = () => {
       // data structure for path finding
-      for (var i = 0; i < COLUMNS; i++) {
-        for (var j = 0; j < ROWS; j++) {
-          map[i][j] = 0;
+      for (var row = 0; row < this.ROWS; row++) {
+        for (var col = 0; col < this.COLUMNS; col++) {
+          this.map[row][col] = 0;
         }
       }
       // graphic objects
-      towerGroup.removeAll();
-      pathGroup.removeAll();
+      this.towerGroup.removeAll();
+      this.pathGroup.removeAll();
   }
 
-  insertTower = (row, col) => {
-    let tower = towerGroup.create(row * this.game.CELL_WIDTH, col * this.game.CELL_HEIGHT, 'tower');
+  insertTower = (col, row) => {
+    const [x, y] = this.cellsToPixels(col, row);
+    let tower = this.towerGroup.create(x, y, 'tower');
     tower.inputEnabled = true;
     tower.events.onInputUp.add(this.handleTowerTap, this);
-    towerGroup.children[towerGroup.length - 1].width = this.game.CELL_WIDTH - 3;
-    towerGroup.children[towerGroup.length - 1].height = this.game.CELL_HEIGHT - 3;
+    this.towerGroup.children[this.towerGroup.length - 1].width = this.game.CELL_WIDTH - 3;
+    this.towerGroup.children[this.towerGroup.length - 1].height = this.game.CELL_HEIGHT - 3;
   }
 
   handleTowerTap = (tower) => {
-    const row = Math.floor( tower.x / this.game.CELL_WIDTH );
-    const column = Math.floor( tower.y / this.game.CELL_HEIGHT );
+    const [col, row] = this.pixelsToCells(tower.x, tower.y);
     tower.destroy();
-    map[row][column] = 0;
+    this.map[row][col] = 0;
   }
 
   // helper method, only used to show path while debugging
-  insertPathPoint = (row, col) => {
+  insertPathPoint = (col, row) => {
     const offset = this.game.CELL_WIDTH / 2 - (this.game.CELL_WIDTH - 40) / 2;
-    pathGroup.create(row * this.game.CELL_WIDTH + offset, col * this.game.CELL_HEIGHT + offset, 'tower');
-    pathGroup.children[pathGroup.length - 1].width = this.game.CELL_WIDTH - 40;
-    pathGroup.children[pathGroup.length - 1].height = this.game.CELL_HEIGHT - 40;
+    const [x, y] = this.cellsToPixels(col, row);
+    this.pathGroup.create(x + offset, y + offset, 'tower');
+    this.pathGroup.children[this.pathGroup.length - 1].width = this.game.CELL_WIDTH - 40;
+    this.pathGroup.children[this.pathGroup.length - 1].height = this.game.CELL_HEIGHT - 40;
+  }
+
+  cellsToPixels = (col, row) => {
+    return [col * this.game.CELL_WIDTH, row * this.game.CELL_HEIGHT];
+  }
+
+  pixelsToCells = (x, y) => {
+    return [Math.floor(x / this.game.CELL_WIDTH),
+            Math.floor(y / this.game.CELL_HEIGHT)];
   }
 
 }
